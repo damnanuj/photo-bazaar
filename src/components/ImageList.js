df4b87a3e860060a5c76d30e64f5276af776fec4
Anuj <anujkrgupta21@gmail.com>
2023-12-26 01:41:34 +0530

    .env added

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,23 +1,11 @@
+const ImageList = ({ images }) => {
+  return (
+    <div className="imagesList">
+      {images.map((item) => (
+        <img src={item.urls.thumb} alt={item.alt_description} />
+      ))}
+    </div>
+  );
+};
 
-
-
-
-
-
-
-
-const ImageList = ({images}) => {
-    return (
-        <div className="imagesList">
-            {
-                images.map(item => (
-                   <img 
-                   src={item.urls.thumb} alt={item.alt_description}
-                   /> 
-                ))
-            }
-        </div>
-    )
-}
-
-export default ImageList
+export default ImageList;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 
 
-
 import React from "react";
 import logo from "../imgs/photBlogo.png"
 
