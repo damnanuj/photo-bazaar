197b1119ab98053830fee4e6d43f677e5677ac2d
Anuj <anujkrgupta21@gmail.com>
2024-10-13 23:35:45 +0530

    default query change

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ImageSearch from "./components/ImageSearch";
 import ImageList from "./components/ImageList";
 import NavBar from "./components/NavBar";
 import Pagination from "./components/Pagination";
 
-
-const App=()=> {
-
-  const [images, setImages] = useState([])
+const App = () => {
+  const [images, setImages] = useState([]);
   // const [fetchimg, setFetchImg] = useState("")
 
   return (
     <div>
-
       <div className="topBar">
-        <NavBar/>
-        <ImageSearch setImages={setImages} 
-        images ={images}/>
-      </div> 
-      <ImageList images={images}/>
+        <NavBar />
+        <ImageSearch setImages={setImages} images={images} />
+      </div>
+      <ImageList images={images} />
 
-      <Pagination/>
-      
-      
+      <Pagination />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,84 +1,79 @@
-
-
-
-
-
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 // console.log(process.env.REACT_APP_UNSPLASH_ACCESS_KEY);
 
-const ImageSearch = ({images, setImages, setFetchImg}) => {
-    const [searchItem , setSearchItem] = useState("")
-    const [page, setPage] = useState(1);
+const ImageSearch = ({ images, setImages, setFetchImg }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [page, setPage] = useState(1);
 
-    useEffect (()=>{
-        
-        fetchImages()   //first param null to =>e
-    },[])
+  useEffect(() => {
+    fetchImages(); //first param null to =>e
+  }, []);
 
-    useEffect(()=>{
-        setPage(1)
-    },[searchItem])    //making the page 1 again if new search
+  useEffect(() => {
+    setPage(1);
+  }, [searchItem]); //making the page 1 again if new search
 
-    async function fetchImages(e , flag){
-        console.log(e)
-        console.log(flag);
-        // console.log(initialInput);
-        if(e){
-            e.preventDefault()
-        }
-        try{
-                const response = await axios.get("https://api.unsplash.com/search/photos", {
-                headers :{
-                    "Accept-Version": "v1",
-                    "Authorization": `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
-                },
-                params:{
-                    query: searchItem || "random",
-                    per_page : 8,
-                    page : page
-                }
-            })
-            // console.log(response.data.results);
-            if(flag ==="submit"){
-                setImages(response.data.results)
-                // setPage(page+1)
-            }
-            else{
-            // setImages(response.data.results)
-                setImages([...images , ...response.data.results])
-                // setPage(page+1)
-            }
-            setPage(page+1)
-        }
-
-
-        catch(error){
-            console.log(error);
-        }    
+  async function fetchImages(e, flag) {
+    console.log(e);
+    console.log(flag);
+    // console.log(initialInput);
+    if (e) {
+      e.preventDefault();
     }
-    return (
-        
-        <div className="searchForm">
+    try {
+      const response = await axios.get(
+        "https://api.unsplash.com/search/photos",
+        {
+          headers: {
+            "Accept-Version": "v1",
+            Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+          },
+          params: {
+            query: searchItem || "cats",
+            per_page: 10,
+            page: page,
+          },
+        }
+      );
+      // console.log(response.data.results);
+      if (flag === "submit") {
+        setImages(response.data.results);
+        // setPage(page+1)
+      } else {
+        // setImages(response.data.results)
+        setImages([...images, ...response.data.results]);
+        // setPage(page+1)
+      }
+      setPage(page + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="searchForm">
+      <form onSubmit={(e) => fetchImages(e, "submit")}>
+        <input
+          className="inputBox"
+          type="text"
+          placeholder="Search your image..."
+          onChange={(e) => setSearchItem(e.target.value)}
+          value={searchItem}
+        />
+        <button className="srchBtn" type="submit">
+          Search
+        </button>
+      </form>
 
-            <form onSubmit={(e)=>fetchImages(e, "submit")}>
-                <input className="inputBox" type="text" placeholder="Search your image..."
-                onChange={(e)=>setSearchItem(e.target.value)}
-                value={searchItem}
-                />
-                <button className="srchBtn" type="submit">Search</button>
-            </form>
+      <div className="pagesBtn">
+        {/* <button className="back">Previous</button> */}
+        <button className="next" onClick={fetchImages}>
+          <i class="fa-solid fa-forward"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-            <div className="pagesBtn">
-                {/* <button className="back">Previous</button> */}
-                <button className="next"
-                   onClick={fetchImages}><i class="fa-solid fa-forward"></i></button>
-            </div>
-        </div>
-
-        
-    )
-}
-
-export default ImageSearch
+export default ImageSearch;
